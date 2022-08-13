270ba8330759f68f980a3cb317eef913c5faa91b
Bruno Oliveira <brunocosta1699320@gmail.com>
2022-08-12 21:18:43 -0300

    2

---
+++ Funcoes/funcao1.js
@@ -0,0 +1,5 @@
+function somar (b,a) {return a+b;}  /* Essa e uma funcao simples, aonde somar a + b que dara o resultado */
+
+console.log(somar(9,9))
+console.log(somar(2,3))
+console.log(somar(56,12))
---
+++ Funcoes/funcao3.js
@@ -0,0 +1,11 @@
+function somar(n){
+    if (n + 2 <= 12){ /* Se n + 2 for maior que 12 */
+        return 'True!' /* Se for maior vai retorna false */
+    } else {
+        return 'False!' /* Se for menor vai retornar True */
+    }
+}
+
+console.log(somar(99))
+
+
